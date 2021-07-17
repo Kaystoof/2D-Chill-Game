f439b35e05d5c19ffc18dd989568cb69a9e80d48
Kaystoof <KrOzKid@gmail.com>
2021-07-17 10:40:56 +1000

    Added all tiles to a list etc.

--- Chill 2D Platformer/Assets/Scripts/TileTemplateGenerator.cs
+++ Chill 2D Platformer/Assets/Scripts/TileTemplateGenerator.cs
@@ -18,10 +18,16 @@ public class TileTemplateGenerator : MonoBehaviour {
     public Vector3 playerSpawnPos;
     public float offsetPos;
 
+    private List<GameObject> allTiles = new List<GameObject>();
+    private List<GameObject> edgeTiles = new List<GameObject>();
+
     private ExitGenerator exitGenerator;
 
 	// Use this for initialization
 	void Start () {
+
+        
+
 
         exitGenerator = GetComponent<ExitGenerator>();
 
@@ -29,6 +35,8 @@ public class TileTemplateGenerator : MonoBehaviour {
         {
             for(int j = 0; j < gridSize; j++)
             {
+
+
                 SpawnEmpty(i,j);
                 
             }
@@ -36,13 +44,13 @@ public class TileTemplateGenerator : MonoBehaviour {
 
         SpawnPlayer();
 
-        exitGenerator.SpawnExit(offsetPos,maxTileDist);
-        
+        //exitGenerator.SpawnExit(offsetPos,maxTileDist);
+        FindEdges();
 
         
 	}
 	
-
+    //player spawn
     void SpawnPlayer()
     {
         playerSpawnPos = new Vector3(0, 0, 0);
@@ -51,22 +59,48 @@ public class TileTemplateGenerator : MonoBehaviour {
 
    
 
-
+    //spawns empty object with a level randomiser script
     void SpawnEmpty(int x, int y)
     {
+        
         //Calculates distance to move whole grid based off number of tiles and size of tiles
         offsetPos = (gridSize-1)*maxTileDist*0.5f;
 
         instantiatePos = new Vector3(x * maxTileDist - offsetPos, y * maxTileDist-offsetPos);
-        Instantiate(emptyGenerator, instantiatePos, Quaternion.identity);
+        GameObject g = Instantiate(emptyGenerator, instantiatePos, Quaternion.identity);
+        allTiles.Add(g);
+       
 
-        
-
-        
     }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void FindEdges()
+    {
+
+        float edgeValuePos = gridSize*(maxTileDist/2);
+        float edgeValueNeg = -edgeValuePos;
+
+        for(int i = 0; i < allTiles.Count; i++)
+        {
+
+            if (allTiles[i].transform.position.x == edgeValueNeg 
+               || allTiles[i].transform.position.x == edgeValuePos)
+            {
+                edgeTiles.Add(allTiles[i]);
+            }
+            else if (allTiles[i].transform.position.y == edgeValueNeg
+               || allTiles[i].transform.position.y == edgeValuePos)
+            {
+                edgeTiles.Add(allTiles[i]);
+            }
+        }
+
+        SpawnExit();
+
+    }
+
+
+    private void SpawnExit()
+    {
+
+    }
 }
